df06b01fe05d4f4cb767fae9b958f14802c997ca
alexandru <109861946+magic-doggo@users.noreply.github.com>
2023-10-30 07:27:35 +0000

    Create function to display output of number button presses

--- calculatorScript.js
+++ calculatorScript.js
@@ -13,3 +13,20 @@ const operate = function (number1, operator, number2){
   }
 } // do I need to create separate functions for each operator and then call them here?
 
+let firstNumber = "";
+let secondNumber = "";
+const button1 = document.querySelector("#one");
+const button2 = document.querySelector("#two");
+let displayScreen = document.querySelector("#display");
+const plusButton = document.querySelector("#plus");
+
+const anyButton = document.querySelectorAll(".button");
+anyButton.forEach(ravioliEvent => {
+  ravioliEvent.addEventListener("click", clickButton);
+  function clickButton() {
+    firstNumber += ravioliEvent.textContent;
+    displayScreen.textContent += ravioliEvent.textContent;
+    // console.log(firstNumber)
+  }
+});
+
